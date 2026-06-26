0fabc7d5f98e6ecdf99dc16f62da312b2b2979f5
KevDev7 <96637803+KevDev7@users.noreply.github.com>
2026-06-26 11:07:17 -0500

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.py
@@ -0,0 +1,26 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+
+        if len(s) != len(t):
+            return False
+
+        s_ht = {}
+        t_ht = {}
+
+        for ch in s:
+            if ch not in s_ht:
+                s_ht[ch] = 1
+            else:
+                s_ht[ch] = s_ht[ch] + 1
+
+        for ch in t:
+            if ch not in t_ht:
+                t_ht[ch] = 1
+            else:
+                t_ht[ch] = t_ht[ch] + 1
+
+        return s_ht == t_ht
+
+
+
+        
